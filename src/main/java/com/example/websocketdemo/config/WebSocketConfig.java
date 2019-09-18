e2b77e55280f549fc1d5ec92dc31dc94cc44c271
kalleh <wallbomk@amazon.com>
2019-09-18 22:26:47 +0200

    fix: added netty ssl client

--- src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
+++ src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
@@ -10,7 +10,6 @@ import org.springframework.web.socket.config.annotation.*;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").withSockJS();
@@ -18,14 +17,21 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+        ReactorNettyTcpClient<byte[]> client = new ReactorNettyTcpClient<>(builder -> { 
+            builder.port(61614)
+                .host("b-d2a3dd17-48b5-4840-b83b-39db24a55f8f-1.mq.eu-west-1.amazonaws.com")
+                .sslSupport(opts -> {})
+        }, new StompReactorNettyCodec());
+
         registry.setApplicationDestinationPrefixes("/app");
 
         // registry.enableSimpleBroker("/topic");   // Enables a simple in-memory broker
         //   Use this for enabling a Full featured broker like RabbitMQ
         registry.enableStompBrokerRelay("/topic")
-                .setRelayHost("stomp+ssl://b-d2a3dd17-48b5-4840-b83b-39db24a55f8f-1.mq.eu-west-1.amazonaws.com")
+                .setRelayHost("b-d2a3dd17-48b5-4840-b83b-39db24a55f8f-1.mq.eu-west-1.amazonaws.com")
                 .setRelayPort(61614)
                 .setClientLogin("test")
                 .setClientPasscode("Adm1nistrator!");
+                .setTcpClient(client);
     }
 }
