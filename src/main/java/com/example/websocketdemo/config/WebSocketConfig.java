4eea9b07c3ff2a7d3755f7ab2ba9893ce0dccd6f
kalleh <wallbomk@amazon.com>
2019-09-18 09:17:00 +0200

    refactor: change to amazon mq

--- src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
+++ src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
@@ -19,17 +19,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");   // Enables a simple in-memory broker
 
-
+        // registry.enableSimpleBroker("/topic");   // Enables a simple in-memory broker
         //   Use this for enabling a Full featured broker like RabbitMQ
-
-        /*
         registry.enableStompBrokerRelay("/topic")
-                .setRelayHost("localhost")
-                .setRelayPort(61613)
-                .setClientLogin("guest")
-                .setClientPasscode("guest");
-        */
+                .setRelayHost("b-b62b1307-b845-4a49-bda1-ee4a2c29d2f1-1.mq.eu-west-1.amazonaws.com")
+                .setRelayPort(61617)
+                .setClientLogin("test")
+                .setClientPasscode("Adm1nistrator!");
     }
 }
