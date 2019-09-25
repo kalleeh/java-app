1fd972fc9a870717d90f6caa5b181d47bdf6622d
kalleh <wallbomk@amazon.com>
2019-09-25 10:54:34 +0200

    fix: change to double quotes

--- src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
+++ src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
@@ -36,8 +36,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/app");
 
         registry.enableStompBrokerRelay("/topic")
-                .setClientLogin('test')
-                .setClientPasscode('Adm1nistrator!')
+                .setClientLogin("test")
+                .setClientPasscode("Adm1nistrator!")
                 .setTcpClient(client);
     }
 }
