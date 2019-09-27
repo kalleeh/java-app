7846b68783ae1de90db6cd6668a9fd00df9c2ed9
kalleh <wallbomk@amazon.com>
2019-09-27 16:15:35 +0200

    fix: increase security

--- src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
+++ src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
@@ -16,7 +16,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("https://gurum-java-app.gurum.cloud/*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+        
     }
 
     @Override
